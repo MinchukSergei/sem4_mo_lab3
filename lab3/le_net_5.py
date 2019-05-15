5d4af0ae0f12ecf65ea98a169b3e7c5cd4daca8d
Sergey Minchuk <minchuk94@gmail.com>
2019-05-15 20:59:04 +0300

    refactored the code

--- lab3/le_net_5.py
+++ lab3/le_net_5.py
@@ -1,282 +1,208 @@
 import tensorflow as tf
-import numpy as np
 from lab1 import data_shuffle
 
-DATA_TR_X = 'tr_x'
-DATA_TR_Y = 'tr_y'
-DATA_V_X = 'v_x'
-DATA_V_Y = 'v_y'
-DATA_TE_X = 'te_x'
-DATA_TE_Y = 'te_y'
-HIDDEN_FC_LAYER_WEIGHT = 'HFCL'
-OUT_FC_LAYER_WEIGHT = 'OFCL'
-PRINT_TRAIN_OUTPUT = True
-DISPLAY_FREQUENCY = 100
-
-# Hyper Params
-HP_BATCH_SIZE = 'batch_size'
-HP_LEARNING_RATE = 'learning_rate'
-HP_UNITS = 'units'
-HP_L1_SCALE = 'l1_scale'
-HP_L2_SCALE = 'l2_scale'
-HP_DROPOUT_RATE = 'dropout_rate'
-HP_DECAY_RATE = 'decay_rate'
-HP_EPOCHS = 'epochs'
-HP_ACT_FUNC = 'act_func'
+DISPLAY_FREQUENCY = 300
+tensor_board_path = 'D:/Programming/bsuir/sem4/MO/lab3/tensorboard'
 
 
-class LeNet5:
-    def __init__(self, data, in_size, out_size):
-        self.tr_x = data[DATA_TR_X]
-        self.tr_y = data[DATA_TR_Y]
-        self.v_x = data[DATA_V_X]
-        self.v_y = data[DATA_V_Y]
-        self.te_x = data[DATA_TE_X]
-        self.te_y = data[DATA_TE_Y]
+class Lenet5:
+    def __init__(self, data, in_size, out_size, epochs):
+        self.data = data
 
         self.in_size = in_size
         self.out_size = out_size
 
-        self.use_adaptive_lr = False
-        self.use_l1_reg = False
-        self.use_l2_reg = True
-        self.use_dropout = True
+        self.epochs = epochs
 
-    def init_sess(self):
-        self.sess = tf.InteractiveSession()
+    def build_model(self):
+        self.x = tf.placeholder(tf.float32, shape=[None, self.in_size], name='IN')
+        self.y = tf.placeholder(tf.float32, shape=[None, self.out_size], name='OUT')
+        self.drop_rate = tf.placeholder(tf.float32)
 
-    def run_sess(self):
-        self.sess.run(self.init)
+        to_nwhc = tf.reshape(self.x, [-1, 28, 28, 1])
 
-    def init_in_out_layers(self):
-        self.nn_in = tf.placeholder(tf.float32, shape=[None, self.in_size], name='IN')
-        self.nn_out = tf.placeholder(tf.float32, shape=[None, self.out_size], name='OUT')
+        layer1 = conv_layer(to_nwhc, 1, 6, [5, 5], [1, 1], 'HCNV1', self.drop_rate, padding='SAME')
 
-    def init_nn(self):
-        self.init_in_out_layers()
+        layer2 = max_pool(layer1, [2, 2])
 
-        self.init_layers()
-        self.init_loss()
-        self.init_learning_rate()
-        self.init_optimizer()
+        layer3 = conv_layer(layer2, 6, 16, [5, 5], [1, 1], 'HCNV2', self.drop_rate, padding='VALID')
 
-        self.correct_prediction = tf.equal(tf.argmax(self.output_logits, 1), tf.argmax(self.nn_out, 1),
-                                           name='correct_prediction')
-        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32), name='accuracy')
+        layer4 = max_pool(layer3, [2, 2])
 
-        self.init = tf.global_variables_initializer()
+        flatten = tf.reshape(layer4, [-1, 5 * 5 * 16])
 
-    def init_hp(self, hp):
-        self.learning_rate = hp[HP_LEARNING_RATE]
-        self.units = hp[HP_UNITS]
-        self.l1_scale = hp[HP_L1_SCALE]
-        self.l2_scale = hp[HP_L2_SCALE]
-        self.batch_size = hp[HP_BATCH_SIZE]
-        self.dropout_rate = hp[HP_DROPOUT_RATE]
-        self.decay_rate = hp[HP_DECAY_RATE]
-        self.epochs = hp[HP_EPOCHS]
-        self.act_func = hp[HP_ACT_FUNC]
-        self.train_size = len(self.tr_y)
+        layer5 = full_connected_layer(flatten, 120, 'HFC1', self.drop_rate)
 
-    def init_layers(self):
-        in_nn_reshaped = tf.reshape(self.nn_in, [-1, 28, 28, 1])
+        layer6 = full_connected_layer(layer5, 84, 'HFC2', self.drop_rate)
 
-        k_conv_size = [5, 5]
-        k_pool_size = [2, 2]
+        self.logits = full_connected_layer(layer6, self.out_size, 'OFC1', self.drop_rate * 0, output=True)
 
-        l1 = self.conv_layer(in_nn_reshaped, 1, 6, k_conv_size, f'{HIDDEN_FC_LAYER_WEIGHT}0', 'SAME', 0)
+        l2_reg = 0.001
+        loss1 = tf.nn.l2_loss(get_tensor_by_name('W_HCNV1')) * l2_reg
+        loss2 = tf.nn.l2_loss(get_tensor_by_name('W_HCNV2')) * l2_reg
 
-        l2 = self.avg_pool(l1, k_pool_size, 'VALID')
+        loss3 = tf.nn.l2_loss(get_tensor_by_name('W_HFC1')) * l2_reg
+        loss4 = tf.nn.l2_loss(get_tensor_by_name('W_HFC2')) * l2_reg
 
-        l3 = self.conv_layer(l2, 6, 16, k_conv_size, f'{HIDDEN_FC_LAYER_WEIGHT}1', 'VALID', 1)
+        regularizer = loss1 + loss2 + loss3 + loss4
 
-        l4 = self.avg_pool(l3, k_pool_size, 'VALID')
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.y, logits=self.logits))
+        self.loss = loss + regularizer
 
-        fc1 = self.full_connected_layer(l4, self.units[0], f'{HIDDEN_FC_LAYER_WEIGHT}2', 2)
+        self.global_step = tf.Variable(0, trainable=False)
+        learn_rate = tf.train.exponential_decay(0.02, self.global_step, len(self.data[0][0]), 0.91)
 
-        fc2 = self.full_connected_layer(fc1, self.units[1], f'{HIDDEN_FC_LAYER_WEIGHT}3', 3)
-
-        self.output_logits = self.full_connected_layer(fc2, self.out_size, OUT_FC_LAYER_WEIGHT, output=True)
-
-    def init_loss(self):
-        loss_function = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.nn_out, logits=self.output_logits),
-            name='loss_function'
-        )
-
-        l1_penalty = 0
-        l2_penalty = 0
-
-        weights = []
-        for i in range(4):
-            weights.append(get_tensor_by_name(f'W_HFCL{i}'))
-        weights.append(get_tensor_by_name('W_OFCL'))
-
-        if self.use_l1_reg:
-            w_penalty = 0
-            for w in weights:
-                w_penalty += l1_loss(w)
-            l1_penalty = self.l1_scale * w_penalty
-
-        if self.use_l2_reg:
-            w_penalty = 0
-            for w in weights:
-                w_penalty += tf.nn.l2_loss(w)
-            l2_penalty = self.l2_scale * w_penalty
-
-        self.loss_function = loss_function + l1_penalty + l2_penalty
-
-    def init_learning_rate(self):
-        global_step = None
-        learn_rate = self.learning_rate
-
-        if self.use_adaptive_lr:
-            global_step = tf.Variable(0, trainable=False)
-
-            learn_rate = tf.train.exponential_decay(
-                self.learning_rate,
-                global_step,
-                self.train_size,
-                self.decay_rate,
-                staircase=True
-            )
-
-        self.learn_rate = learn_rate
-        self.global_step = global_step
-
-    def init_optimizer(self):
-        optimizer = tf.train.GradientDescentOptimizer(
-            learning_rate=self.learn_rate
+        self.optimizer = tf.train.GradientDescentOptimizer(
+            learning_rate=learn_rate
         ).minimize(
-            loss=self.loss_function,
+            loss=self.loss,
             global_step=self.global_step
         )
 
-        self.optimizer = optimizer
+        self.prediction = tf.argmax(self.logits, 1)
+        self.correct_prediction = tf.equal(tf.argmax(self.logits, 1), tf.argmax(self.y, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
 
-    def avg_pool(self, input_data, pool_shape, padding):
-        ksize = [1, pool_shape[0], pool_shape[1], 1]
-        strides = [1, 2, 2, 1]
-        return tf.nn.avg_pool(input_data, ksize=ksize, strides=strides, padding=padding)
-
-    def conv_layer(self, input_data, num_input_channels, num_filters, filter_shape, name, padding, layer_number=None):
-        conv_filt_shape = [filter_shape[0], filter_shape[1], num_input_channels, num_filters]
-
-        W = weight_variable(name, conv_filt_shape)
-        b = bias_variable(name, [num_filters])
-
-        out_layer = tf.nn.conv2d(input_data, W, [1, 1, 1, 1], padding=padding) + b
-
-        out_layer = tf.nn.relu(out_layer)
-
-        if self.use_dropout:
-            out_layer = tf.nn.dropout(out_layer, rate=self.dropout_rate[layer_number])
-
-        return out_layer
-
-    def full_connected_layer(self, x, num_units, name, layer_number=None, output=False):
-        in_dim = 1
-
-        for i in x.shape:
-            if i.value is not None:
-                in_dim *= i.value
-
-        W = weight_variable(name, shape=[in_dim, num_units])
-        b = bias_variable(name, [num_units])
-
-        flattened_x = tf.reshape(x, [-1, in_dim])
-        layer = tf.matmul(flattened_x, W) + b
-
-        if not output:
-            layer = self.act_func(layer)
-
-        if self.use_dropout and not output:
-            layer = tf.nn.dropout(layer, rate=self.dropout_rate[layer_number])
-
-        return layer
-
-    def fit(self):
+    def fit_model(self):
+        batch_size = 128
         global_step = 0
 
-        loss_function = self.loss_function
-        accuracy = self.accuracy
-        batch_size = self.batch_size
-        sess = self.sess
-        nn_in = self.nn_in
-        nn_out = self.nn_out
+        (x_train, y_train), (x_valid, y_valid) = self.data[:-1]
 
-        num_tr_iter = int(len(self.tr_y) / batch_size)
-        num_v_iter = int(len(self.v_y) / batch_size)
+        num_train_iter = int(len(y_train) / batch_size)
+        num_valid_iter = int(len(y_valid) / batch_size)
 
-        best_score = {
-            'accuracy': 0,
-            'loss': 0
-        }
+        best_acc = 0
+
+        self.init = tf.global_variables_initializer()
+        self.session = tf.InteractiveSession()
+        self.session.run(self.init)
+
+        tf.summary.scalar('accuracy', self.accuracy)
+        tf.summary.scalar('loss', self.loss)
+
+        self.merged = tf.summary.merge_all()
+        self.train_writer = tf.summary.FileWriter(tensor_board_path + '/train', self.session.graph)
+        self.valid_writer = tf.summary.FileWriter(tensor_board_path + '/valid')
+
+        tf.global_variables_initializer().run()
 
         for epoch in range(self.epochs):
             print('Training epoch: {}'.format(epoch + 1))
 
-            tr_x, tr_y = data_shuffle(self.tr_x, self.tr_y)
-            v_x, v_y = data_shuffle(self.v_x, self.v_y)
+            tr_x, tr_y = data_shuffle(x_train, y_train)
+            v_x, v_y = data_shuffle(x_valid, y_valid)
 
-            for iteration in range(num_tr_iter):
+            for iteration in range(num_train_iter):
                 global_step += 1
                 start = iteration * batch_size
                 end = (iteration + 1) * batch_size
                 x_batch, y_batch = get_next_batch(tr_x, tr_y, start, end)
 
                 feed_dict_batch = {
-                    nn_in: x_batch,
-                    nn_out: y_batch
+                    self.x: x_batch,
+                    self.y: y_batch,
+                    self.drop_rate: 0.15
                 }
 
-                self.sess.run(self.optimizer, feed_dict=feed_dict_batch)
+                _, summary = self.session.run([self.optimizer, self.merged], feed_dict=feed_dict_batch)
+                self.train_writer.add_summary(summary, global_step)
 
                 if iteration % DISPLAY_FREQUENCY == 0:
-                    loss_batch, acc_batch = sess.run([loss_function, accuracy], feed_dict=feed_dict_batch)
+                    loss_batch, acc_batch = self.session.run([self.loss, self.accuracy], feed_dict=feed_dict_batch)
 
-                    if PRINT_TRAIN_OUTPUT:
-                        print("iter {0:3d}:\t TRAIN Loss={1:.2f},\t Accuracy={2:.01%}".format(iteration, loss_batch,
-                                                                                              acc_batch))
-            correct_predictions = []
-            for iteration in range(num_v_iter):
+                    print("iter {0:3d}:\t TRAIN Loss={1:.2f},\t Accuracy={2:.01%}".format(iteration, loss_batch,
+                                                                                          acc_batch))
+
+            acc_valid = 0
+            for iteration in range(num_valid_iter):
                 start = iteration * batch_size
                 end = (iteration + 1) * batch_size
                 x_batch, y_batch = get_next_batch(v_x, v_y, start, end)
 
-                feed_dict_valid = {
-                    nn_in: x_batch,
-                    nn_out: y_batch
+                feed_dict_batch = {
+                    self.x: x_batch,
+                    self.y: y_batch,
+                    self.drop_rate: 0
                 }
 
-                correct_prediction = sess.run([self.correct_prediction], feed_dict=feed_dict_valid)
-                correct_predictions.extend(correct_prediction[0])
+                acc, summary = self.session.run([self.accuracy, self.merged], feed_dict=feed_dict_batch)
+                acc_valid += acc
 
-            acc_valid = sess.run(tf.reduce_mean(tf.cast(correct_predictions, tf.float32)))
+                self.valid_writer.add_summary(summary, global_step)
 
-            if acc_valid > best_score['accuracy']:
-                best_score['accuracy'] = acc_valid
+            acc_valid = acc_valid / num_valid_iter
 
-            if PRINT_TRAIN_OUTPUT:
-                print('---------------------------------------------------------')
-                print("Epoch: {0}:\t VALID Accuracy={1:.01%}".format(epoch + 1, acc_valid))
-                print('---------------------------------------------------------')
+            if acc_valid > best_acc:
+                best_acc = acc_valid
 
-        return best_score
+            print('---------------------------------------------------------')
+            print("Epoch: {0}:\t VALID Accuracy={1:.01%}".format(epoch + 1, acc_valid))
+            print('---------------------------------------------------------')
 
-    def test_nn(self):
-        feed_dict_valid = {
-            self.nn_in: self.te_x,
-            self.nn_out: self.te_y
-        }
+    def test_model(self):
+        (x_test, y_test) = self.data[2]
 
-        loss_valid, acc_valid = self.sess.run([self.loss_function, self.accuracy], feed_dict=feed_dict_valid)
+        batch_size = 256
+        num_test_iter = int(len(x_test) / batch_size)
+
+        acc_valid = 0
+        global_step = 0
+        for iteration in range(num_test_iter):
+            global_step += 1
+            start = iteration * batch_size
+            end = (iteration + 1) * batch_size
+            x_batch, y_batch = get_next_batch(x_test, y_test, start, end)
+
+            feed_dict_batch = {
+                self.x: x_batch,
+                self.y: y_batch,
+                self.drop_rate: 0
+            }
+
+            acc = self.session.run([self.accuracy], feed_dict=feed_dict_batch)
+            acc_valid += acc[0]
 
         print('---------------------------------------------------------')
-        print("TEST Loss: {0:.2f}, Accuracy: {1:.01%}".format(loss_valid, acc_valid))
+        print("TEST Accuracy={0:.01%}".format(acc_valid / num_test_iter))
         print('---------------------------------------------------------')
 
-        return acc_valid, loss_valid
+
+def max_pool(input_data, pool_shape):
+    ksize = [1, pool_shape[0], pool_shape[1], 1]
+    strides = [1, 2, 2, 1]
+    return tf.nn.max_pool(input_data, ksize=ksize, strides=strides, padding='SAME')
+
+
+def conv_layer(input_data, num_input_channels, num_filters, filter_shape, strides, name, dropout=0, padding='SAME'):
+    conv_filt_shape = [filter_shape[0], filter_shape[1], num_input_channels, num_filters]
+
+    W = weight_variable(name, conv_filt_shape)
+    b = bias_variable(name, [num_filters])
+
+    out_layer = tf.nn.conv2d(input_data, W, [1, *strides, 1], padding=padding) + b
+
+    out_layer = tf.nn.relu(out_layer)
+
+    out_layer = tf.nn.dropout(out_layer, rate=dropout)
+
+    return out_layer
+
+
+def full_connected_layer(x, num_units, name, dropout=0, output=False):
+    in_dim = x.get_shape()[1]
+
+    W = weight_variable(name, shape=[in_dim, num_units])
+    b = bias_variable(name, [num_units])
+
+    layer = tf.matmul(x, W) + b
+
+    if not output:
+        layer = tf.nn.relu(layer)
+
+    layer = tf.nn.dropout(layer, rate=dropout)
+
+    return layer
 
 
 def get_tensor_by_name(name):
@@ -284,7 +210,7 @@ def get_tensor_by_name(name):
 
 
 def weight_variable(name, shape):
-    initial = tf.truncated_normal_initializer(stddev=0.01)
+    initial = tf.truncated_normal_initializer(stddev=0.1)
 
     return tf.get_variable('W_' + name,
                            dtype=tf.float32,
@@ -293,7 +219,7 @@ def weight_variable(name, shape):
 
 
 def bias_variable(name, shape):
-    initial = tf.constant(0., shape=shape, dtype=tf.float32)
+    initial = tf.constant(1, shape=shape, dtype=tf.float32)
 
     return tf.get_variable('b_' + name,
                            dtype=tf.float32,
@@ -305,11 +231,3 @@ def get_next_batch(data_x, data_y, start, end):
     y_batch = data_y[start:end]
 
     return x_batch, y_batch
-
-
-def l1_loss(w):
-    return tf.reduce_sum(tf.abs(w))
-
-
-def reset_graph():
-    tf.reset_default_graph()
--- lab3/main.py
+++ lab3/main.py
@@ -1,115 +1,26 @@
-import numpy as np
-import tensorflow as tf
 from lab1 import small_data, large_data, split_data, get_unique_data, encode_classes, generate_one_hot_encoded_class
 
-from lab3.conv_nn import HP_BATCH_SIZE, HP_LEARNING_RATE, HP_UNITS, HP_L1_SCALE, HP_L2_SCALE, ConvNN
-from lab3.conv_nn import DATA_TR_X, DATA_TR_Y, DATA_V_X, DATA_TE_X, DATA_TE_Y, DATA_V_Y
-from lab3.conv_nn import HP_DROPOUT_RATE, HP_DECAY_RATE, HP_ACT_FUNC, HP_EPOCHS
-from lab3.grid_search import GridSearch
-from lab3.conv_nn import reset_graph
-from lab3.le_net_5 import LeNet5
-
-RND_SEED = None
-tf.random.set_random_seed(RND_SEED)
-np.random.seed(RND_SEED)
-
 # Img props
+from lab3.conv_nn import ConvNN
+from lab3.le_net_5 import Lenet5
+
 img_w = 28
 img_h = 28
 img_size = img_h * img_w
 img_classes = 10
 
-# # Learning hyper params
-epochs = 100
-batch_size = 100
-learning_rate = 0.001
-units = [300, 300]
-
-# # L1 Regularization
-use_l1_regularization = True
-l1_scale = 0.001
-
-# # L2 Regularization
-use_l2_regularization = True
-l2_scale = 0.001
-
-# # Dropout
-use_dropout = True
-dropout_rate = 0.15
-
-# # Adaptive lr
-use_adaptive_lr = True
-decay_rate = 0.95
-
 
 def main():
-    tr_x, tr_y, v_x, v_y, te_x, te_y = prepare_data(small_data)
-
-    # fc_nn = ConvNN(
-    #     {
-    #         DATA_TR_X: tr_x,
-    #         DATA_TR_Y: tr_y,
-    #         DATA_V_X: v_x,
-    #         DATA_V_Y: v_y,
-    #         DATA_TE_X: te_x,
-    #         DATA_TE_Y: te_y
-    #     },
-    #     img_size,
-    #     img_classes
-    # )
-
-    # grid_search = GridSearch(
-    #     fc_nn,
-    #     {
-    #         HP_BATCH_SIZE: [300],
-    #         HP_LEARNING_RATE: [0.003],
-    #         HP_UNITS: [512],
-    #         HP_L1_SCALE: [0],
-    #         HP_L2_SCALE: [0.00005],
-    #         HP_DROPOUT_RATE: [[0.05, 0.1, 0.4]],
-    #         HP_DECAY_RATE: [0.80],
-    #         HP_ACT_FUNC: [tf.nn.relu],
-    #         HP_EPOCHS: [100]
-    #     },
-    #     fit_model,
-    #     'accuracy',
-    #     1
-    # )
-
-    lenet5 = LeNet5(
-        {
-            DATA_TR_X: tr_x,
-            DATA_TR_Y: tr_y,
-            DATA_V_X: v_x,
-            DATA_V_Y: v_y,
-            DATA_TE_X: te_x,
-            DATA_TE_Y: te_y
-        },
+    nn = Lenet5(
+        prepare_data(small_data),
         img_size,
-        img_classes
+        img_classes,
+        epochs=50
     )
 
-    grid_search = GridSearch(
-        lenet5,
-        {
-            HP_BATCH_SIZE: [128],
-            HP_LEARNING_RATE: [0.005],
-            HP_UNITS: [[120, 84]],
-            HP_L1_SCALE: [0],
-            HP_L2_SCALE: [0.0001],
-            HP_DROPOUT_RATE: [[0.1, 0.2, 0.3, 0.4]],
-            HP_DECAY_RATE: [0.85],
-            HP_ACT_FUNC: [tf.nn.relu],
-            HP_EPOCHS: [1000]
-        },
-        fit_model,
-        'accuracy',
-        1
-    )
-
-    grid_search.execute()
-
-    print(grid_search.get_results())
+    nn.build_model()
+    nn.fit_model()
+    nn.test_model()
 
 
 def prepare_data(path_to_data):
@@ -118,25 +29,13 @@ def prepare_data(path_to_data):
     images, labels = get_unique_data(path_to_data)
     labels = encode_classes(labels, one_hot_encoded_labels)
 
-    tr_x, tr_y, v_x, v_y, te_x, te_y = split_data(images, labels, 0.8, 0.1, 0.1)
-    tr_x = tr_x.reshape(tr_x.shape[0], -1)
-    v_x = v_x.reshape(v_x.shape[0], -1)
-    te_x = te_x.reshape(te_x.shape[0], -1)
+    x_train, y_train, x_valid, y_valid, x_test, y_test = split_data(images, labels, 0.92, 0.03, 0.05)
 
-    return tr_x, tr_y, v_x, v_y, te_x, te_y
+    x_train = x_train.reshape(x_train.shape[0], -1)
+    x_valid = x_valid.reshape(x_valid.shape[0], -1)
+    x_test = x_test.reshape(x_test.shape[0], -1)
 
-
-def fit_model(nn, hp):
-    reset_graph()
-
-    nn.init_hp(hp)
-    nn.init_nn()
-    nn.init_sess()
-    nn.run_sess()
-
-    best_score = nn.fit()
-    # fc_nn.test_nn()
-    return best_score
+    return (x_train / 255, y_train), (x_valid / 255, y_valid), (x_test / 255, y_test)
 
 
 if __name__ == '__main__':
